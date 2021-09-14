1953a828c241e745885af3d2e34beb8c6319943e
Filip <milovanovicfilip007@gmail.com>
2021-09-14 23:44:14 +0200

    Website TItle

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import GlobalStyle from "./GlobalStyles";
+import useEffect from "react";
 
 function App() {
+  useEffect(() => {
+    document.title = "Webull Mockup";
+  }, []);
   return (
     <>
       <GlobalStyle />
